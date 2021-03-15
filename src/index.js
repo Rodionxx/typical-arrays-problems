b10b569c86800e449741d292963563bc386ce772
rodionxx <rodionxx@gmail.com>
2021-03-15 09:35:17 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (Array.isArray(array) && array.length ){
+    let minumal = array[0];
+    array.forEach(element => {
+      if (element < minumal) {
+        minumal = element;
+      }
+    });
+    return minumal;
+  } else {return 0;}
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (Array.isArray(array) && array.length ){
+    let maximal = array[0];
+    array.forEach(element => {
+      if (element > maximal) {
+        maximal = element;
+      }
+    });
+    return maximal;
+
+  } else {
+  return 0;}
 }
 
 exports.avg = function avg (array) {
-  return 0;
-}
+  if (Array.isArray(array) && array.length ){
+    let total = 0;
+  array.forEach(element => {
+    total = total + element;
+  });
+  return (total / array.length);
+} else {return 0;}}
